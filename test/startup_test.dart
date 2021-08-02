3ca67580d3cf303f1e331c8997dd8df3aca8ffba
Archie <oluwatstar21@gmail.com>
2021-08-02 11:43:44 +0100

    Refactor: fix test

--- test/startup_test.dart
+++ test/startup_test.dart
@@ -1,6 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
 import 'package:ii_test/app/locator.dart';
+import 'package:ii_test/core/models/user.dart';
+import 'package:ii_test/core/models/user_list_response.dart';
+import 'package:ii_test/core/services/user_service.dart';
+import 'package:ii_test/core/utils/hive_boxes.dart';
 import 'package:ii_test/core/utils/logger.dart';
+import 'package:mockito/mockito.dart';
+
+class MockUserService extends Mock implements UserService {}
+
+class MockHive extends Mock implements HiveInterface {}
+class MockRegistry extends Mock implements TypeRegistry {}
+
+class MockBox<T> extends Mock implements Box<T> {}
+
+void initialiseHive() async {
+  var path = Directory.current.path;
+  Hive
+    ..init(path);
+}
 
 void main() {
   test('Setup is working', () async{
@@ -9,8 +30,21 @@ void main() {
     locator.allowReassignment = true;
     setupLogger(test: true);
 
-    await setupLocator(test: true);
+    final mockUserService = MockUserService();
 
+    final userMockBox = MockBox<User>();
+
+    final mockhive = MockHive();
+
+    when(mockhive.isBoxOpen(HiveBox.user)).thenAnswer((_) => false);
+
+    when(mockhive.openBox<User>(HiveBox.user))
+        .thenAnswer((_) async => Future.value(userMockBox));
+
+    locator.registerSingleton<HiveInterface>(mockhive);
+    locator.registerSingleton<UserService>(mockUserService);
+
+    await setupLocator(test: true);
     await locator.allReady();
   });
 }
